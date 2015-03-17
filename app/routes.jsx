89dae3dde8235d5adeb0b8ad5945648c6cf1c6d7
Nicolas Hery <hery.nicolas@gmail.com>
2015-03-17 17:40:26 -0400

    Update dependencies

--- app/routes.jsx
+++ app/routes.jsx
@@ -1,16 +1,18 @@
 var React = require('react');
-// Suppress ESLint "no-unused-vars" warning
-window.React = React;
 var Router = require('react-router');
 var Route = Router.Route;
 var Redirect = Router.Redirect;
 var NotFoundRoute = Router.NotFoundRoute;
 
-module.exports = [
-  <Route path="/app" handler={require('./components/App.jsx')}>
-    <Route name="tasks" handler={require('./components/TaskHistory.jsx')}/>
-    <Route name="task-new" path="task/new" handler={require('./components/NewTask.jsx')}/>
-    <Redirect from="/app" to="task-new" />
-  </Route>,
-  <NotFoundRoute name="not-found" handler={require('./components/NotFound.jsx')}/>
-];
+// Wrap in function to temporarily fix ESLint warning
+// https://github.com/yannickcr/eslint-plugin-react/issues/19
+module.exports = (function() {
+  return [
+    <Route path="/app" handler={require('./components/App.jsx')}>
+      <Route name="tasks" handler={require('./components/TaskHistory.jsx')}/>
+      <Route name="task-new" path="task/new" handler={require('./components/NewTask.jsx')}/>
+      <Redirect from="/app" to="task-new" />
+    </Route>,
+    <NotFoundRoute name="not-found" handler={require('./components/NotFound.jsx')}/>
+  ];
+})();
